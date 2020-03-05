0f9ad9fd0990265dc6a9b6921db6dfb3f0f8d949
yuji_imuta <y.imuta@axelmark.co.jp>
2020-03-05 12:20:26 +0900

    python でcsv ファイルの差分を確認するスクリプト

---
+++ text_check.py
@@ -0,0 +1,29 @@
+import difflib as diff
+import pandas as pd
+import os
+
+path = os.getcwd()
+
+print('読み込みたいcsvファイルを書いてください')
+print('↓'*10)
+a = input()
+
+df1 = pd.read_csv(a)
+
+print('読み込みたいcsvファイルを書いてください')
+print('↓'*10)
+b = input()
+
+df2 = pd.read_csv(b)
+
+df_diff = df1[~df1['word'].isin(df2['word'])]
+
+
+
+print('新しいcsvのファイル名を書いてくださいß')
+c = input()
+
+df_diff.to_csv(c)
+
+
+
